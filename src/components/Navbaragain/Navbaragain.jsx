fedaafc4014d6c6cfdb17f29d34f4cc478c6e92d
shahadot99999 <shahadot99999@gmail.com>
2025-05-07 11:34:47 +0600

    Responsive Navbar using react and tailwind

---
+++ src/components/Navbaragain/Navbaragain.jsx
@@ -0,0 +1,26 @@
+import Link from "../Link/Link";
+
+const Navbaragain = () => {
+
+   const routes = [
+  { id: 1, path: "/", name: "Home" },
+  { id: 2, path: "/about", name: "About Us" },
+  { id: 3, path: "/products", name: "Products" },
+  { id: 4, path: "/contact", name: "Contact" },
+  { id: 5, path: "/profile", name: "User Profile" },
+];
+    return (
+        <nav>
+           <ul className="md:flex">
+           {
+                routes.map(route => <Link
+                    key={route.id}
+                    route={route}
+                ></Link>)
+            }
+           </ul>
+        </nav>
+    );
+};
+
+export default Navbaragain;
